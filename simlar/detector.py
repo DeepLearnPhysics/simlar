c79531722d924bfcd1bc363a19ddc73bb3dde40d
seanxia8 <seanxia8@gmail.com>
2025-09-25 10:59:29 -0700

    Make sure PMTs are at least 1 PMT radius away from the detector edges

--- simlar/detector.py
+++ simlar/detector.py
@@ -1,6 +1,8 @@
 import torch
 
-def generate_pmt_positions(lx, ly, lz, spacing_y, spacing_z, gap_pmt_active):
+def generate_pmt_positions(
+    lx, ly, lz, spacing_y, spacing_z, gap_pmt_active, n_pmt_walls=2, pmt_radius=50,
+):
     """
     Generate PMT positions in a hexagonal grid pattern.
     Parameters
@@ -17,6 +19,9 @@ def generate_pmt_positions(lx, ly, lz, spacing_y, spacing_z, gap_pmt_active):
         Spacing between PMTs in the z-direction.
     gap_pmt_active : float
         Gap between the PMT and the active volume.
+    n_pmt_walls: int
+        Number of walls to mount PMT (default is 2).
+    pmt_radius: float
     Returns
     -------
     pmt_coords : torch.Tensor
@@ -24,36 +29,68 @@ def generate_pmt_positions(lx, ly, lz, spacing_y, spacing_z, gap_pmt_active):
     pmt_ids : torch.Tensor
         Tensor containing the IDs of the PMTs.
     """
-
-    grid_y = int(ly / spacing_y)
-    grid_z = int(lz / spacing_z)
-    print(f"Total PMT number is {2 * grid_y * grid_z}")
+    
+    grid_y = int(ly / spacing_y) + 1
+    grid_z = int(lz / spacing_z) + 1
 
     # Generate hexagonal grid coordinates
-    y_side, z_side = torch.meshgrid(torch.arange(grid_y), torch.arange(grid_z))
-    y_side = y_side * spacing_y - ly / 2
-    z_side = z_side * spacing_z - lz / 2
-    y_side = y_side.to(torch.float32)
-    z_side = z_side.to(torch.float32)
+    spacing_buffer_y = ly - (grid_y-1)*spacing_y - 4*pmt_radius
+    spacing_buffer_z = lz - (grid_z-1)*spacing_z - 4*pmt_radius
+    while spacing_buffer_y < spacing_y/2:
+        grid_y -= 1
+        spacing_buffer_y = ly - (grid_y - 1) * spacing_y
+    while spacing_buffer_z < 0:
+        grid_z -= 1
+        spacing_buffer_z = ly - (grid_z - 1) * spacing_z
+    print(f"Spacing buffer in y: {spacing_buffer_y}, z: {spacing_buffer_z}")
+
+    y_side, z_side = np.meshgrid(np.arange(grid_y), np.arange(grid_z), indexing="ij")
+    y_side = y_side*spacing_y - ly/2 + (spacing_buffer_y/2 - spacing_y/4) + pmt_radius
+    z_side = z_side*spacing_z - lz/2 + spacing_buffer_z/2 + 2*pmt_radius
+
+    print(f"Total PMT number is {n_pmt_walls * grid_y * grid_z}")
+
+    y_side = y_side.astype(np.float32)
+    z_side = z_side.astype(np.float32)
+
+    
     for i in range(y_side.shape[1]):
         if i % 2 == 1:
-            y_side[:, i] += spacing_y / 2
+            y_side[:, i] += spacing_y / 2 - pmt_radius
 
-    y = torch.tile(y_side, (2, 1, 1)).flatten()#reshape(2, -1)
-    z = torch.tile(z_side, (2, 1, 1)).flatten()#reshape(2, -1)
+    y = np.tile(y_side, (n_pmt_walls, 1, 1)).flatten()  # reshape(2, -1)
+    z = np.tile(z_side, (n_pmt_walls, 1, 1)).flatten()  # reshape(2, -1)
 
-    num_lo_pmt = num_hi_pmt = int(len(y)/2)
-    lo_x_value = -lx/2-gap_pmt_active
-    hi_x_value = lx/2+gap_pmt_active
-    x = torch.concat((torch.full((num_lo_pmt,), lo_x_value),torch.full((num_hi_pmt,), hi_x_value)))
+    if n_pmt_walls == 2:
+        num_lo_pmt = num_hi_pmt = int(len(y) / 2)
+        lo_x_value = -lx / 2 - gap_pmt_active
+        hi_x_value = lx / 2 + gap_pmt_active
+        x = np.concatenate(
+            (
+                np.full((num_lo_pmt,), lo_x_value),
+                np.full((num_hi_pmt,), hi_x_value),
+            )
+        )
+        normal = np.zeros((len(x), 3), dtype=np.float32)
+        normal[:num_lo_pmt, 0] = 1.0  # -x side PMTs face +x direction
+        normal[num_lo_pmt:, 0] = -1.0  # +x side PMTs face -x direction
+    elif n_pmt_walls == 1:
+        num_pmt = len(y)
+        hi_x_value = lx + gap_pmt_active
+        x = np.full((num_pmt,), hi_x_value)  # Single wall PMT at x=lx+gap_pmt_active
+        normal = np.zeros((len(x), 3), dtype=np.float32)
+        normal[:, 0] = 1.0  # +x side PMTs face +x direction
+    else:
+        raise ValueError("Number of PMT walls must be 1 or 2.")
+
     # print(x, y, z)
     # Shift every other row to create a hexagonal pattern
 
-    #pmt_coords = torch.column_stack((x.flatten(), y.flatten(), z.flatten()))
-    # change to swap between the sides
-    pmt_coords = torch.stack((x,y,z), dim=-1)
-    return pmt_coords, torch.arange( pmt_coords.shape[0], dtype=torch.int32)
+    # pmt_coords = torch.column_stack((x.flatten(), y.flatten(), z.flatten()))
 
+    # change to swap between the sides
+    pmt_coords = np.stack((x, y, z), axis=-1)
+    return pmt_coords, np.arange(pmt_coords.shape[0], dtype=np.int32), normal
 
 def pmt_collection_efficiency(x: torch.Tensor, **kwargs):
     '''
--- simlar/models.py
+++ simlar/models.py
@@ -20,13 +20,14 @@ class PhotonTransport:
         '''
         self.light_yield = config['PHYSICS']['light_yield']
         self.cathode_thickness = config['GEOMETRY']['TPC']['cathode_thickness']
+        self.n_pmt_walls = config['GEOMETRY']['PMT'].get('n_pmt_walls', 2)
         self.active_xrange = config['GEOMETRY']['TPC']['active_volume']['x']
         self.c = config['PHYSICS']['light_speed']
         self.c /= config['PHYSICS']['lar_refraction_index']
         self.time_resolution = config['SIMULATION']['TRUTH']['photon_time_resolution']
         self.ns2bin = 0.001 / self.time_resolution
         self.sigmoid_coeff = config['GEOMETRY']['PMT']['ce_angle_thres']
-        self.mean_pe_threshold = config['SIMULATION']['TRUTH']['mean_pe_threshold']
+        self.mean_pe_threshold = config['SIMULATION']['TRUTH'].get('mean_pe_threshold', 0)
         self.use_ang_acc = config['GEOMETRY']['PMT'].get('use_ang_acc', False)
         self.device = 'cpu'
         self.debug_mode = config.get('DEBUG', False)
@@ -38,11 +39,14 @@ class PhotonTransport:
         lz = lz[1] - lz[0]
         spacing = config['GEOMETRY']['PMT']['sensor_spacing']
         self.gap_pmt_active = config['GEOMETRY']['PMT']['gap_pmt_active']
-        self.pmt_positions, self.pmt_ids= generate_pmt_positions(lx=lx,
-            ly=ly,lz=lz,
-            spacing_y=spacing,spacing_z=spacing,
-            gap_pmt_active=self.gap_pmt_active)
         self.sensor_radius = config['GEOMETRY']['PMT']['sensor_radius']
+        self.pmt_positions, self.pmt_ids = generate_pmt_positions(lx=lx,
+                                                                  ly=ly,lz=lz,
+                                                                  spacing_y=spacing,spacing_z=spacing,
+                                                                  gap_pmt_active=self.gap_pmt_active, n_pmt_walls=self.n_pmt_walls,
+                                                                  pmt_radius = self.sensor_radius
+                                                                  
+        )
 
     def to(self,device):
         '''
@@ -78,7 +82,7 @@ class PhotonTransport:
         '''
         return de * self.light_yield
 
-    def get_pe(self, num_photons,points):
+    def get_pe(self, num_photons, points):
         '''
         Function to calculate the number of photoelectrons detected by PMTs.
         Parameters
@@ -96,11 +100,19 @@ class PhotonTransport:
         torch.Tensor
             A tensor of shape (P,) containing the number of photoelectrons detected at each PMT and time bin.
         '''
-        
-        pos_mask_v = [(points[:,0]>  self.cathode_thickness/2.) & (points[:,0]<(self.active_xrange[1]+self.gap_pmt_active)),
-                    (points[:,0]<(-self.cathode_thickness/2.)) & (points[:,0]>(self.active_xrange[0]-self.gap_pmt_active))]
 
-        pmt_mask_v = [self.pmt_positions[:,0]>self.cathode_thickness/2., self.pmt_positions[:,0]<(-self.cathode_thickness/2.)]
+        if self.n_pmt_walls == 2:
+            pos_mask_v = [(points[:,0]> self.cathode_thickness/2.) & (points[:,0]<(self.active_xrange[1]+self.gap_pmt_active)),
+                        (points[:,0]<(-self.cathode_thickness/2.)) & (points[:,0]>(self.active_xrange[0]-self.gap_pmt_active))]
+
+            pmt_mask_v = [self.pmt_positions[:,0]>self.cathode_thickness/2., self.pmt_positions[:,0]<(-self.cathode_thickness/2.)]
+
+        elif self.n_pmt_walls == 1:
+            pos_mask_v = [(points[:,0]>self.cathode_thickness/2.) & (points[:,0]<(self.active_xrange[1]+self.gap_pmt_active))]
+            pmt_mask_v = [self.pmt_positions[:,0]>self.cathode_thickness/2.]
+
+        else:
+            raise ValueError("Number of PMT walls must be 1 or 2.")
 
         pmt_data=[]
         res_id_v,res_t_v,res_n_v = [],[],[]
@@ -136,7 +148,7 @@ class PhotonTransport:
         else:
             return None, None, None
 
-    def survived_photon2pmts(self, nph, pos, pmt_pos):
+    def survived_photon2pmts(self, nph, pos, pmt_pos, return_acc=False):
         '''
             Function to calculate the number of photoelectrons detected by PMTs.
             Parameters
@@ -145,6 +157,10 @@ class PhotonTransport:
                 A tensor of shape (N,) containing the number of photons emitted from each point.
             points : torch.Tensor
                 A tensor of shape (N, 4) containing the (x, y, z, time) coordinates of each photon emission point.
+            pmt_pos : torch.Tensor
+                A tensor of shape (M, 3) containing the PMT coordinates.
+            return_acc : bool
+                If True, returns the acceptance rate of photons for each PMT.
             Returns
             -------
             nph_survived: torch.Tensor
@@ -158,12 +174,15 @@ class PhotonTransport:
         # to verify tof need the float value, otherwise all 0
         #tof = (r.T / self.c + pos[:, 3]).T
 
-        ce = pmt_collection_efficiency(arccos, sigmoid_coeff=self.sigmoid_coeff)
-        if not self.use_ang_acc:
+        if self.use_ang_acc:
+            ce = pmt_collection_efficiency(arccos, sigmoid_coeff=self.sigmoid_coeff)
+            number_frac *= ce
+
+        if not return_acc:
             n_survived = nph.unsqueeze(1) * number_frac
+            return n_survived, tof
         else:
-            n_survived = nph.unsqueeze(1) * number_frac * ce
-        return n_survived, tof
+            return number_frac, tof
 
     def propagate_photon2pmts(self, photon_pos, pmt_pos):
         '''
